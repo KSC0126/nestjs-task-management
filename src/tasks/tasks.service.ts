93be345be8329dabc07a3c33d20ae4403d16d780
showrya <kollushowrya@gmail.com>
2020-02-10 20:44:37 -0500

    creating task
    created a task inside a service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import * as uuid from 'uuid/v1';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +8,16 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks; // since getAllTasks is with in the class it has access to tasks array even though it is defined as private
+    }
+
+    createTask(title: string, description: string): Task {
+        const task: Task = {
+            id: uuid(),
+            title: title,
+            description: description,
+            status: TaskStatus.OPEN
+        }
+        this.tasks.push(task);
+        return task; // by returning a newly created object front end don't need make an extra call to know which object is created
     }
 }
