bb0aa3726388cdcd61d05b7c55d42489d2360177
showrya <kollushowrya@gmail.com>
2020-02-10 20:23:58 -0500

    service-controller
    Establishing a connection bewteen a service and controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) { }
 
+    @Get() // letting nest js know it is a get class or using @Get decorator to make get call
+    getAllTasks() {
+        return this.tasksService.getAllTasks(); // return a response with tasks array
+    }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService { }
+export class TasksService {
+    private tasks = []; // an tasks array to access from the controller will be fetching from database later
+
+    getAllTasks() {
+        return this.tasks; // since getAllTasks is with in the class it has access to tasks array even though it is defined as private
+    }
+}
