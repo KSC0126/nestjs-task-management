7ae2fd009d09201e6e43a8ff172f3e7d72d240e1
showrya <kollushowrya@gmail.com>
2020-02-10 20:33:05 -0500

    task model
    1. Created a interface for task and added to service and controller to have them strictly typed

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) { }
 
     @Get() // letting nest js know it is a get class or using @Get decorator to make get call
-    getAllTasks() {
+    getAllTasks(): Task[] {
         return this.tasksService.getAllTasks(); // return a response with tasks array
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
 
 @Injectable()
 export class TasksService {
-    private tasks = []; // an tasks array to access from the controller will be fetching from database later
+    private tasks: Task[] = []; // an tasks array to access from the controller will be fetching from database later
 
-    getAllTasks() {
+    getAllTasks(): Task[] {
         return this.tasks; // since getAllTasks is with in the class it has access to tasks array even though it is defined as private
     }
 }
