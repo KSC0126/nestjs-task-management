fc37f006cbc141f7f304e59b234054340031eabc
showrya <kollushowrya@gmail.com>
2020-02-10 21:00:03 -0500

    create task from controller
    created a task from the controller
    got to know how to use @Body decorator

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
@@ -9,5 +9,13 @@ export class TasksController {
     @Get() // letting nest js know it is a get class or using @Get decorator to make get call
     getAllTasks(): Task[] {
         return this.tasksService.getAllTasks(); // return a response with tasks array
+    }
+
+    @Post()
+    createTask(
+        @Body('title') title: string,
+        @Body('description') description: string): Task {
+        console.log('description', description);
+        return this.tasksService.createTask(title, description);
     }
 }
