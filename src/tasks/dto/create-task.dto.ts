4b365166c06b70071b50feb4986919ff0e4252fc
showrya <kollushowrya@gmail.com>
2020-02-18 10:36:01 -0500

    addes validation pipes
    pipes: https://github.com/typestack/class-validator#validation-decorators

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,7 @@
+import { IsNotEmpty } from 'class-validator';
 export class CreateTaskDto {
+    @IsNotEmpty() // by decorating it with IsNotEmpty we can make sure that title is not empty 
     title: string;
+    @IsNotEmpty()
     description: string;
 }
